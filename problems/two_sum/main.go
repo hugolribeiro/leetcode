c6eded160082347190a1cf0817977bdbb4a573d8
Hugo Ribeiro <hugolerib@gmail.com>
2022-03-10 22:44:10 -0300

    Added fater version to Two Sum problem

--- problems/two_sum/main.go
+++ problems/two_sum/main.go
@@ -7,7 +7,31 @@ func main() {
 	fmt.Println(twoSum(numbers, 6))
 }
 
-// Slower version
+// Faster Version (0ms)
+func twoSum(nums []int, target int) []int {
+	values := make(map[int]int, len(nums))
+	for index, num := range nums {
+		if indx, ok := values[target-num]; ok {
+			return []int{index, indx}
+		}
+		values[num] = index
+	}
+	return []int{}
+}
+
+// Medium version (4ms)
+// func twoSum(nums []int, target int) []int {
+// 	values := make(map[int]int)
+// 	for index, num := range nums {
+// 		if indx, ok := values[target-num]; ok {
+// 			return []int{index, indx}
+// 		}
+// 		values[num] = index
+// 	}
+// 	return []int{}
+// }
+
+// Slower version (40ms)
 // func twoSum(nums []int, target int) []int {
 // 	for i := 0; i < len(nums)-1; i++ {
 // 		for j := i + 1; j < len(nums); j++ {
@@ -18,15 +42,3 @@ func main() {
 // 	}
 // 	return []int{0, 0}
 // }
-
-// Faster version
-func twoSum(nums []int, target int) []int {
-	values := make(map[int]int)
-	for index, num := range nums {
-		if indx, ok := values[target-num]; ok {
-			return []int{index, indx}
-		}
-		values[num] = index
-	}
-	return []int{}
-}
